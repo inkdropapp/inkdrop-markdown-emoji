3d54f21e5900c702811ca193b80ff1d64b54973a
Takuya Matsuyama <nora@odoruinu.net>
2016-09-17 20:39:15 +0900

    Gemoji short-code support

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const emoji = require('remark-gemoji-to-emoji')
+const gemoji = require('remark-gemoji')
 
 module.exports = {
   activate () {
     const MDEPreview = inkdrop.components.classes.MDEPreview
-    return MDEPreview.remarkPlugins.push(emoji)
+    return MDEPreview.remarkPlugins.push(gemoji, emoji)
   },
   deactivate () {
     MDEPreview.remarkPlugins = MDEPreview.remarkPlugins.filter(function (plugin) {
