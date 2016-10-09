4557c0594afc4b44d0cf90504bba995b46aed306
Takuya Matsuyama <nora@odoruinu.net>
2016-10-09 12:19:13 +0900

    Avoid failing initialization

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const gemoji = require('remark-gemoji')
 
 module.exports = {
   activate () {
-    const MDEPreview = inkdrop.components.classes.MDEPreview
-    return MDEPreview.remarkPlugins.push(gemoji, emoji)
+    const { MDEPreview } = inkdrop.components.classes
+    if (MDEPreview) {
+      return MDEPreview.remarkPlugins.push(gemoji, emoji)
+    }
   },
   deactivate () {
-    MDEPreview.remarkPlugins = MDEPreview.remarkPlugins.filter(function (plugin) {
-      return plugin !== emoji && plugin !== gemoji
-    })
+    const { MDEPreview } = inkdrop.components.classes
+    if (MDEPreview) {
+      MDEPreview.remarkPlugins = MDEPreview.remarkPlugins.filter(function (plugin) {
+        return plugin !== emoji && plugin !== gemoji
+      })
+    }
   }
 }
